e86752df06cc7e820da7dd7a447bae6d0a080b88
andrewbenson <andrew.benson@grahambrown.com>
2018-12-05 14:21:59 +0000

    Adding Lesson 1 and 2

---
+++ Learning Unity/Assets/Lesson 1/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour {
+
+    public string yourName; // Because it's public you can see this in the Unity Inspector
+    public int age = 10; // This is a private variable, so you cannot see this in the Unity Inspector
+
+    void Start()
+    {
+        Debug.Log("Hello " + yourName);
+        Debug.Log("Your age is " + age);
+        Debug.Log("Your position is" + transform.position);
+    }
+}
---
+++ Learning Unity/Assets/Lesson 2 - Translation Rotation/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 3f;
+    public float turnSpeed = 40; 
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate( Vector3.forward * speed * Time.deltaTime );
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate( Vector3.back * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
+        }
+    }
+}
