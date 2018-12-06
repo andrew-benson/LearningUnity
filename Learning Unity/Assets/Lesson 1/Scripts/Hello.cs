4e149ca503866a9d05eaaae8d0905c29de278b76
andrewbenson <andrew.benson@grahambrown.com>
2018-12-06 10:15:03 +0000

    step back to private

--- Learning Unity/Assets/Lesson 1/Scripts/Hello.cs
+++ Learning Unity/Assets/Lesson 1/Scripts/Hello.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Hello : MonoBehaviour {
 
     public string yourName; // Because it's public you can see this in the Unity Inspector
-    public int age = 10; // This is a private variable, so you cannot see this in the Unity Inspector
+    private int age = 10; // This is a private variable, so you cannot see this in the Unity Inspector
 
     void Start()
     {
         Debug.Log("Hello " + yourName);
         Debug.Log("Your age is " + age);
-        Debug.Log("Your position is" + transform.position);
+        Debug.Log("Your position is " + transform.position);
     }
 }
